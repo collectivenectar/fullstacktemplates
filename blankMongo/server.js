0c54c74dae540203790ba7d0755bea798e0a8d50
Jonathan Harvey <jonathanharvey22@gmail.com>
2022-07-04 16:30:50 +0200

    added mongoDB connection and PORT listening

--- blankMongo/server.js
+++ blankMongo/server.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const MongoClient = requre('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'sample_airbnb',
+    collection
+
+MongoClient.connect(dbConnectionString)
+  .then(client => {
+    console.log("connected to MongoDB database soundslip")
+    db = client.db(dbName)
+    collection = db.collection("listingsAndReviews")
+  })
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`your server is running on port ${process.env.PORT || PORT}`)
+})
