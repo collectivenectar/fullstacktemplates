541ae052f9c2bac4ef73fee9af2e3e5948026996
Jonathan Harvey <jonathanharvey22@gmail.com>
2022-07-09 15:16:32 +0200

    added public and views plus changes to server.js

---
+++ blankMongo/public/main.js
@@ -0,0 +1,2 @@
+// This is where you put code that requests from the server, use fetch directed at
+// mongoDB app
--- blankMongo/server.js
+++ blankMongo/server.js
@@ -16,6 +16,12 @@ MongoClient.connect(dbConnectionString)
     collection = db.collection("listingsAndReviews")
   })
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
 app.listen(process.env.PORT || PORT, () => {
   console.log(`your server is running on port ${process.env.PORT || PORT}`)
 })
